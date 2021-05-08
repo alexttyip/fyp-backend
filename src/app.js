9ec379271360a784a5be56fcee140e17e025e47a
Yip, Alex (UG - Computer Science) <ty00208@surrey.ac.uk>
2021-05-08 17:58:11 +0100

    Use Yarn classic, added mongodb basic code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const { DB_USER, DB_PW, CLUSTER_URL } = process.env;
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PW}@${CLUSTER_URL}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    await client.connect();
+
+    console.log("Connected to MongoDB");
+
+    const database = client.db("vmv");
+    const voters = database.collection("voters");
+    res.json(
+      await voters
+        .find(
+          {},
+          {
+            projection: { _id: 0, voter_id: 1 },
+          }
+        )
+        .toArray()
+    );
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 });
 
 app.listen(port, () => {
